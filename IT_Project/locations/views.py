eaadfd04423128bb238ed28de0c517e189d8e595
Shihan Luo <2728798L@student.gla.ac.uk>
2023-03-06 15:29:33 +0000

    webpage

--- IT_Project/locations/views.py
+++ IT_Project/locations/views.py
@@ -23,30 +23,31 @@ class LocationView(CreateView):
 
 def register(request):
     if request.method == "POST":
-        cust_name = request.POST.get("cust_name").strip()
+        fullname = request.POST.get("fullname").strip()
         username = request.POST.get("username").strip()
         passwd = request.POST.get("passwd").strip()
+        userRole = request.POST.get("userRole").strip()
 
         with sqlite3.connect("./db.sqlite3") as db:
             cursor = db.cursor()
-        cursor.execute(
-        """SELECT cust_username
-                From Customer
-                WHERE cust_username= '{}' """.format(username)
+            cursor.execute(
+            """SELECT username
+                    From userlist
+                    WHERE username= '{}' """.format(username)
         )
         res = cursor.fetchone()
         if res:
             db.close()
-            messages.error(request, "The username existed. Reenter, please.")
+            messages.error(request, "Sorry,the username existed.")
             return redirect("/register")
         else:
             cursor.execute(
-                """INSERT INTO customer (cust_username, cust_pw, pwd_salt, cust_name)
-                    VALUES ('{}', '{}', '{}', '{}')""".format(username, passwd, salt, cust_name)
+                """INSERT INTO userlist (fullname, username, passwd, userrole)
+                    VALUES ('{}', '{}', '{}', '{}')""".format(fullname,username, passwd, userRole)
             )
             db.commit()
             db.close()
             # return redirect("/login")
             return redirect("/")
     else:
-        return render(request, "register.html")
+        return render(request, "locations/register.html")
