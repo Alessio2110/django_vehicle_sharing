12e4ba6b0f145a080da3250b76908437b7ed8a35
YutongNi <2731989n@student.gla.ac.uk>
2023-03-18 18:02:03 +0000

    url test

--- IT_Project/locations/tests.py
+++ IT_Project/locations/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+import os,django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse,resolve
+from locations.views import index,return_order,order_history,order_detail,create_order,conclude_order,deposit,payment
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
+
+    def test_return_order_url_is_resolved(self):
+        url = reverse('return_order')
+        self.assertEquals(resolve(url).func, return_order)
+
+    def test_order_history_is_resolved(self):
+        url = reverse('order_history')
+        self.assertEquals(resolve(url).func, order_history)
+
+    def test_order_detail_url_is_resolved(self):
+        url = reverse('order_detail',args=1)
+        self.assertEquals(resolve(url).func, order_detail)
+
+    def test_create_order_is_resolved(self):
+        url = reverse('create_order')
+        self.assertEquals(resolve(url).func, create_order)
+
+    def test_conclude_order_url_is_resolved(self):
+        url = reverse('conclude_order')
+        self.assertEquals(resolve(url).func, conclude_order)
+
+    def test_deposit_url_is_resolved(self):
+        url = reverse('deposit')
+        self.assertEquals(resolve(url).func, deposit)
+
+    def test_payment_url_is_resolved(self):
+        url = reverse('payment',args=1)
+        self.assertEquals(resolve(url).func, payment)
+
+
+
+
