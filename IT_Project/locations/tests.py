e6d508dd4cdba553c862bc27637dede48a8db467
YutongNi <2731989n@student.gla.ac.uk>
2023-03-18 18:14:34 +0000

    url test

--- IT_Project/locations/tests.py
+++ IT_Project/locations/tests.py
@@ -7,6 +7,10 @@ from django.test import SimpleTestCase
 from django.urls import reverse,resolve
 from locations.views import index,return_order,order_history,order_detail,create_order,conclude_order,deposit,payment
 
+from django.test import Client
+from locations.models import Location, CustomUser, VehicleType, Vehicle, Order, ReportType, Report
+import json
+
 
 class TestUrls(SimpleTestCase):
 
@@ -23,7 +27,7 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func, order_history)
 
     def test_order_detail_url_is_resolved(self):
-        url = reverse('order_detail',args=1)
+        url = reverse('order_detail', args=1)
         self.assertEquals(resolve(url).func, order_detail)
 
     def test_create_order_is_resolved(self):
@@ -39,9 +43,17 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func, deposit)
 
     def test_payment_url_is_resolved(self):
-        url = reverse('payment',args=1)
+        url = reverse('payment', args=1)
         self.assertEquals(resolve(url).func, payment)
 
 
+class TestViews(TestCase):
+
+    def test_index_GET(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'locations/home.html')
 
 
